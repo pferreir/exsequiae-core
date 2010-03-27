e69d8db4a334615b1c85ec6e0e4588d361e555d4
Pedro Ferreira <ilzogoiby@gmail.com>
2010-03-27 19:31:18 +0100

    Added some setup info (dependencies) and indented the template.

--- run.py
+++ run.py
@@ -19,4 +19,3 @@ application = app.wsgifunc()
 
 if __name__ == "__main__":
     app.run()
-    
--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ setup(
     name = "exsequiae",
     author = "Pedro Ferreira",
     author_email = "ilzogoiby@gmail.com",
+    description = "A personal publishing system, based on markdown and web.py",
     version = "0.1",
+    install_requires = ['markdown2', 'web.py', 'pycountry'],
     packages = find_packages(),
     package_data = {
     '': ['*.html']
     })
+
